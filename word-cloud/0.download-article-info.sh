49cdda69b52a9c68b535fe1b2b4d0367cf49e58f
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-05-04 00:41:44 +0800

    Download article-info of selected author

---
+++ word-cloud/0.download-article-info.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+
+# Full author name
+fau=${1:-Regev, Aviv}
+author=$(echo $fau | grep -o '^[^,]*' | tr 'A-Z' 'a-z')
+query_string="\"${fau}\"[FAU]"
+echo $query_string
+
+mkdir -p $author
+esearch -db pubmed -query "$query_string" \
+    | efetch -format medline \
+    > ${author}/${author}.txt
