607f0ca63ebc9d03bbd75bec63aa740c5d63db03
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-05-05 23:13:40 +0800

    Search all articles using full author name

--- network.py
+++ network.py
@@ -38,12 +38,19 @@ def get_article_info(pmid):
             'abstract': pm_abstract,
             'author': pm_author}
 
+def search_author(author):
+    query_string = '"' + author + '"[FAU]"'
+    handle = Entrez.esearch(db = 'pubmed', term = query_string, retmax = 100, idtype = 'acc')
+    record = Entrez.read(handle)
+    handle.close()
+    return record['IdList']
+
 def search_coauthor(author1, author_list):
     pmid_list = set()
     for author2 in author_list:
         if author1 == author2: continue
         query_string = '"' + author1 + '"[FAU] AND "' + author2 + '"[FAU]'
-        handle = Entrez.esearch(db = 'pubmed', term = query_string, idtype = 'acc')
+        handle = Entrez.esearch(db = 'pubmed', term = query_string, retmax = 100, idtype = 'acc')
         record = Entrez.read(handle)
         handle.close()
         pmid_list |= set(record['IdList'])
@@ -94,54 +101,66 @@ if __name__ == '__main__':
     import sys
     import re
     import pprint
+    import copy
 
-    if len(sys.argv) > 1 and re.search('[^0-9]', sys.argv[1]) == None:
-        pmid = sys.argv[1]
-        author = None
-        if len(sys.argv) > 2:
-            author = sys.argv[2]
-            if ',' not in author:
-                author = author.split()[-1] + ', ' + author.split()[:-1]
+    if len(sys.argv) > 1:
+        if re.search('[^0-9]', sys.argv[1]) == None:
+            pmid = sys.argv[1]
+            author = None
+            if len(sys.argv) > 2:
+                author = sys.argv[2]
+                if ',' not in author:
+                    author = author.split()[-1] + ', ' + author.split()[:-1]
+        else:
+            pmid = None
+            author = sys.argv[1]
     else:
         sys.stderr.write('Usage  : %s <pubmed id> [author name]\n' % sys.argv[0])
+        sys.stderr.write('         %s <author name>\n' % sys.argv[0])
         sys.stderr.write('Example: %s 27549193 "Li, Bo"\n' % sys.argv[0])
         sys.exit(1)
 
-    article_info = get_article_info(pmid)
-    author_list = article_info['author']
-    print 'Journal : %s' % article_info['journal']
-    print 'Title   : %s' % article_info['title']
-    print 'Abstract: %s' % article_info['abstract']
+    if not pmid == None:
+        article_info = get_article_info(pmid)
+        author_list = article_info['author']
+        coauthor_list = author_list
+        coauthor_list_lower = map(lambda x:x.lower(), coauthor_list)
 
-    if author == None:
-        author = author_list[0]
-    elif author not in author_list:
-        sys.stderr.write('Cannot find author: %s\n' % author)
-        sys.exit(1)
+        print 'Journal : %s' % article_info['journal']
+        print 'Title   : %s' % article_info['title']
+        print 'Abstract: %s' % article_info['abstract']
+
+        if author == None:
+            author = author_list[0]
+        elif author not in author_list:
+            sys.stderr.write('Cannot find author: %s\n' % author)
+            sys.exit(1)
+
+        article_list = search_coauthor(author, author_list)
+    else:
+        coauthor_list = [author]
+        coauthor_list_lower = [author.lower()]
+        article_list = search_author(author)
 
     coauthor_link = {}
-    coauthor_set = set(author_list)
-    for _ in range(1):
-        article_list = search_coauthor(author, author_list)
-        print '\n'.join(article_list)
-        author_list = []
-        for article_id in article_list:
-            article_info = get_article_info(article_id)
-            title = article_info['title']
-            journal = article_info['journal']
-            if article_id not in coauthor_link:
-                coauthor_link[article_id] = article_info
 
-            for coauthor in coauthor_link[article_id]['author']:
-                if coauthor not in coauthor_set and coauthor not in author_list:
-                    author_list.append(coauthor)
+    print '\n'.join(article_list)
+    for article_id in article_list:
+        article_info = get_article_info(article_id)
+        title = article_info['title']
+        journal = article_info['journal']
+        coauthor_link[article_id] = copy.deepcopy(article_info)
+
+        for idx, coauthor in enumerate(article_info['author']):
+            if coauthor not in coauthor_list:
+                if coauthor.lower() in coauthor_list_lower:
+                    coauthor_link[article_id]['author'][idx] = \
+                            coauthor_list[coauthor_list_lower.index(coauthor.lower())]
+                else:
+                    coauthor_list.append(coauthor)
+                    coauthor_list_lower.append(coauthor.lower())
                     print "New coauthor '%s' in article '%s' (%s)" % \
                             (coauthor, title, journal)
-
-        if not author_list:
-            break
-        else:
-            coauthor_set |= set(author_list)
 
     # pprint.pprint(coauthor_link)
     author_node, author_edge = coauthor_link_to_network(coauthor_link)
