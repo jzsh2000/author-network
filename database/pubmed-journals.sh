8a3100ca62e7fbba4528e3b1f3d1b718bd9d85ce
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-05-29 18:04:39 +0800

    Download 'List of All Journals Cited in PubMed'

---
+++ database/pubmed-journals.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ue
+
+cd $(dirname $0)
+
+if [ ! -r J_Medline.txt ]; then
+    # see also: https://www.nlm.nih.gov/bsd/serfile_addedinfo.html
+    wget ftp://ftp.ncbi.nih.gov/pubmed/J_Medline.txt
+fi
+
+echo -e "JrId\tJournalTitle\tMedAbbr\tISSN (Print)\tISSN (Online)\tIsoAbbr\tNlmId" \
+    > J_Medline.tsv
+cat J_Medline.txt \
+    | grep -v '^---' \
+    | sed -e 's/^[^:]*: //' \
+    | paste - - - - - - - \
+    >> J_Medline.tsv
