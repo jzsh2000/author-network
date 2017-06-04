308bccad00f95e81894268f2361eb6a43a962e63
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-06-04 21:31:51 +0800

    Save a new plot as a html file

--- paper/2.plot-author-article.R
+++ paper/2.plot-author-article.R
@@ -3,6 +3,11 @@ library(stringr)
 library(lubridate)
 library(forcats)
 
+library(plotly)
+library(htmlwidgets)
+
+# TODO: Add information of first author/corresponding author
+# TODO: Show article title and number of citations
 plot_article <- function(author) {
     journal_info <- read_csv("../database/journal-IF-medline.csv",
                              col_types = 'cccd') %>%
@@ -16,19 +21,33 @@ plot_article <- function(author) {
     author_journal_info <- author_journal %>%
         inner_join(journal_info,
                    by = c('journal_title' = 'JournalTitle')) %>%
+        mutate(IF_group = fct_rev(factor(ImpactFactor %/% 5 * 5))) %>%
+        mutate(year = year(date)) %>%
         arrange(date)
 
     author_journal_info %>%
-        mutate(IF_group = fct_rev(factor(ImpactFactor %/% 5 * 5))) %>%
-        mutate(year = year(date)) %>%
         ggplot(aes(x = year, y = 1, fill = IF_group)) +
             geom_bar(stat = 'identity') +
             ylab('publications') +
-            scale_x_continuous(breaks = seq(from = 2002, to = 2017, by = 1)) +
+            scale_x_continuous(breaks = full_seq(range(author_journal_info$year),
+                                                 period = 1)) +
             theme_bw()
 
-    ggsave(file.path(author,
-                     paste0(author, '.pdf')))
+    ggsave(filename = file.path(author, paste0(author, '.pdf')))
+
+    author_journal_info %>%
+        rename(journal = journal_title) %>%
+        ggplot(aes(x = year, y = 1, fill = IF_group,
+                   date = date, journal = journal)) +
+            geom_bar(stat = 'identity') +
+            ylab('publications') +
+            scale_x_continuous(breaks = full_seq(range(author_journal_info$year),
+                                             period = 1)) +
+            theme_bw()
+
+    saveWidget(widget = ggplotly(tooltip = c('date', 'journal')),
+               file = file.path(getwd(), author, paste0(author, '.html')))
+
 }
 
 args = commandArgs(trailingOnly = TRUE)
