88a9b071a74ded96c086a0c51d5bcf8b7224ee9b
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-06-04 20:21:04 +0800

    Update plot function

--- paper/1.count-journal.sh
+++ paper/1.count-journal.sh
@@ -3,12 +3,9 @@ set -ueo pipefail
 
 author=${1:-regev}
 cat ${author}/${author}.txt \
-    | grep -e '^TA' -e '^JT ' \
+    | grep -e '^PMID' -e '^TA' -e '^JT ' -e '^EDAT' \
     | cut -c7- \
-    | paste - - \
-    | sort \
-    | uniq -c \
-    | sort -k1,1nr \
-    | gsed -e '1i Count\tTitle Abbreviation\tTitle' -e 's/^  *//' -e 's/ /\t/' \
+    | paste - - - - \
+    | gsed -e "1i pmid\tjournal_title_abbr\tjournal_title\tdate" \
     | tee ${author}/${author}.tsv \
     | csvlook -t
--- paper/2.plot-author-article.R
+++ paper/2.plot-author-article.R
@@ -1,13 +1,32 @@
 library(tidyverse)
+library(stringr)
+library(lubridate)
+library(forcats)
 
 plot_article <- function(author) {
-    read_csv("../database/journal-IF-medline.csv") %>%
-        inner_join(read_tsv(file.path(author,
-                                      paste0(author, '.tsv'))),
-                   by = c('JournalTitle' = 'Title')) %>%
-        ggplot(aes(x = ImpactFactor, y = Count)) +
+    journal_info <- read_csv("../database/journal-IF-medline.csv",
+                             col_types = 'cccd') %>%
+        select(-c(MedAbbr, NlmId))
+
+    author_journal <- read_tsv(file.path(author,
+                                         paste0(author, '.tsv')),
+                               col_types = 'iccc') %>%
+        mutate(date = ymd(str_extract(date, '^[^ ]*')))
+
+    author_journal_info <- author_journal %>%
+        inner_join(journal_info,
+                   by = c('journal_title' = 'JournalTitle')) %>%
+        arrange(date)
+
+    author_journal_info %>%
+        mutate(IF_group = fct_rev(factor(ImpactFactor %/% 5 * 5))) %>%
+        mutate(year = year(date)) %>%
+        ggplot(aes(x = year, y = 1, fill = IF_group)) +
             geom_bar(stat = 'identity') +
+            ylab('publications') +
+            scale_x_continuous(breaks = seq(from = 2002, to = 2017, by = 1)) +
             theme_bw()
+
     ggsave(file.path(author,
                      paste0(author, '.pdf')))
 }
